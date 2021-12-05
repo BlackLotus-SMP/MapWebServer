ce02c27642fed7b411f41994e9856d28ac5a1dfa
kahzerx <kahzerx@tutanota.com>
2021-12-05 13:28:41 +0100

    generic

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ fi
 docker build -t maps .
 chmod +x stop.sh
 ./stop.sh
-docker run -d --name mapscnt -p 80:80 -p 443:443 -v $PWD/maps:/otakucraft/maps maps
-docker exec mapscnt certbot --nginx -d otakucraft.kahzerx.com --agree-tos --register-unsafely-without-email --no-redirect
+docker run -d --name mapscnt -p 80:80 -p 443:443 -v $PWD/maps:/maps maps
+docker exec mapscnt certbot --nginx -d maps.kahzerx.com --agree-tos --register-unsafely-without-email --no-redirect
 docker exec mapscnt service cron start
 docker exec mapscnt crontab crn
